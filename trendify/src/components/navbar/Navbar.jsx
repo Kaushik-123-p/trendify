a59fc207d580bfa1f9ac7ea15cada7206ced4508
kaushik-p-123 <prajapatikaushik6292@gmail.com>
2025-10-31 22:55:54 +0530

    implement addToCart functionality in cartSlice and update ProductCard and Navbar components

--- trendify/src/components/navbar/Navbar.jsx
+++ trendify/src/components/navbar/Navbar.jsx
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
 import { FaShoppingCart } from "react-icons/fa";
 import { FaUser } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
+  const products = useSelector((state) => state.cart.products);
   return (
-    <nav className="bg-white shadow-md">
+    <nav className="bg-white shadow-md static top-0 left-0 w-full z-10">
       <div className="container mx-auto px-4 md:px-16 lg:px-24  py-4 flex justify-between items-center">
         <div className="text-lg font-bold">
           <Link to="/">trendify</Link>
@@ -22,8 +24,13 @@ const Navbar = () => {
           </form>
         </div>
         <div className="flex items-center space-x-4">
-          <Link to="/cart">
+          <Link to="/cart" className="relative">
             <FaShoppingCart size={20} />
+            {products.length > 0 && (
+              <span className="absolute -top-4 -right-5 text-xs w-3 h-3 p-2 left-1 bg-red-600 text-white rounded-full flex items-center justify-center ">
+                {products.length}
+              </span>
+            )}
           </Link>
           <button className="hidden md:block">Login | Register</button>
           <button className="block md:hidden">
--- trendify/src/redux/features/cartSlice.jsx
+++ trendify/src/redux/features/cartSlice.jsx
@@ -2,13 +2,36 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   products: [],
+  totalQuantity: 0,
+  totalPrice: 0,
 };
 
 const cartSlice = createSlice({
   name: "cart",
   initialState,
-  reducers: {},
+  reducers: {
+    addToCart: (state, action) => {
+      const newItem = action.payload;
+      const itemIndex = state.products.find((item) => item.id === newItem.id);
+
+      if (itemIndex) {
+        itemIndex.quantity += 1;
+        itemIndex.totalPrice += newItem.price;
+      } else {
+        state.products.push({
+          id: newItem.id,
+          name: newItem.name,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          image: newItem.image,
+        });
+      }
+      state.totalPrice += newItem.price;
+      state.totalQuantity += 1;
+    },
+  },
 });
 
-export const { setProducts } = cartSlice.actions;
+export const { setProducts, addToCart } = cartSlice.actions;
 export default cartSlice.reducer;
